7862f87cc222c256128eb6ae0a4666fa37e1fa36
Joseph Doiron <57968347+jdoiro3@users.noreply.github.com>
2022-07-17 20:49:05 -0400

    Update script.py

--- src/script.py
+++ src/script.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+class MyClass:
+    """Print print print!"""
+
+    def method_a(self):
+        """Print A!"""
+        print("A!")
+
+    def method_b(self):
+        """Print B!"""
+        print("B!")
+
+    def method_c(self):
+        """Print C!"""
+        print("C!")
